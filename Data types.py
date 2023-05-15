d41ecfb3884d9ade905ab4d2cda1c7c8e7667268
Getange <emmanuelgetange48@gmail.com>
2023-05-15 23:43:35 +0300

    second trial

---
+++ Data types.py
@@ -0,0 +1,12 @@
+#block of code to show indentition in python.
+if 5>2:
+    print("Five is greater than 2")
+    
+ #python has no command for declaring a variable.   
+    x=5
+    y="Hello, world!"
+    print(x)
+    print(y)
+    
+    _variable1="Emmanuel"
+    print(_variable1)
