31b008422d24de0999eeec785e8e9708b24356a9
Getange <emmanuelgetange48@gmail.com>
2023-06-06 13:04:14 +0300

    added python data typea

--- Data types.py
+++ Data types.py
@@ -2,7 +2,7 @@
 if 5>2:
     print("Five is greater than 2")
     
- #python has no command for declaring a variable.   
+#python has no command for declaring a variable.
     x=5
     y="Hello, world!"
     print(x)
@@ -11,4 +11,26 @@ if 5>2:
     _variable1="Emmanuel"
     print(_variable1)
     
-#Data types in python
+# Data types in python
+num1 = 7
+print(num1, 'is of type ', type(num1))
+
+num2 = 2.0
+print(num2, 'is of type ', type(num2))
+
+num3 = 1+2j
+print(num3, 'is of type ', type(num3))
+
+num4='hello world'
+print(num4,'is of type ',type(num4))
+
+languages=['javascript','Python','Swift','Java']
+print("The language at index 3 is : "+languages[3])
+
+# python tuple data types
+product = ('Xbox', 499.99)
+print("The product at index 0 is :"+product[0])
+
+# python set data types
+student_id = {12, 13, 14, 15, 16, 17}
+print(student_id)
