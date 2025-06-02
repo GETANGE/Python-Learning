2d1719d5fbe705ec4e5acfaf775681e52e5d6815
Getange <emmanuelgetange48@gmail.com>
2025-06-02 23:55:15 +0300

    Python Data-types

---
+++ Python Learning/Data-types/data types.py
@@ -0,0 +1,22 @@
+# NUMERIC DATATYPES
+# Integers
+var_1 = 30
+print("Integer :",var_1, type(var_1))
+
+# Floats
+var_2 = 30.2
+print("Float in python: ",var_2, type(var_2))
+
+# complex numbers
+var_3 = 3+5j
+print("Complex number data type",var_3, type(var_3))
+
+# SEQUENCE DATATYPES
+# sequence data types are used to store multiple elements in an ordered manner.
+# LISTS => mutable in nature
+list_1 = [1, 0.5, 'hello', var_3, 'welcome']
+print("Initialized list: ",list_1, type(list_1))
+
+# TUPLES => immutable in nature
+tuple_1 = (1, 0.5, 'hello', var_3, 'welcome')
+print("Initialized tuple: ", tuple_1, type(tuple_1))
--- Python Learning/data types.py
+++
@@ -1,7 +0,0 @@
-print("Hello world!");
-
-for i in range(30):
-    print(i)
-    
-    for j in "Emmanuel":
-        print(j)
